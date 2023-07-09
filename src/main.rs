25f6b45f6224a4de633c115e2f2b04f1351c97df
p3zz <federico.pezzato.work@gmail.com>
2023-07-09 12:19:52 +0200

    add fresh stm32h7xx project

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{hprintln};
+
+#[entry]
+fn main() -> ! {
+    hprintln!("Hello, world!");
+    loop{}
+}
