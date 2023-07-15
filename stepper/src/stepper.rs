66d8c5bb237dd1a85412322ce99813235f508e16
p3zz <federico.pezzato.work@gmail.com>
2023-07-15 11:17:26 +0200

    [WIP] add set_speed and step methods

--- stepper/src/stepper.rs
+++ stepper/src/stepper.rs
@@ -1,11 +1,13 @@
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::timer::CountDown;
+// TODO add embedded_time for duration and time conversions
 
 struct Stepper<O, T>{
     step: O,
     dir: O,
     steps_per_revolution: u32,
     timer: T,
+    step_delay: f32
 }
 
 impl <O, T> Stepper<O, T>
@@ -13,7 +15,20 @@ where O: OutputPin, T: CountDown,
 {
     pub fn new(step: O, dir: O, steps_per_revolution: u32, timer: T) -> Stepper<O,T>{
         Stepper{
-            step, dir, steps_per_revolution, timer
+            step, dir, steps_per_revolution, timer, step_delay: 1000.0
         }
+    }
+
+    pub fn set_speed(&mut self, rpm: u32) -> (){
+        let rps = (rpm / 60) as f32;
+        let spr = 1.0 / rps;
+        self.step_delay = spr / (self.steps_per_revolution as f32);
+    }
+
+    pub fn step(&mut self) -> (){
+        self.timer.start();
+        self.step.set_high();
+        self.timer.wait().unwrap();
+        self.step.set_low();
     }
 }
