e7357bfd04430487d3513a347561e8564f59274f
p3zz <federico.pezzato.work@gmail.com>
2023-07-15 22:33:03 +0200

    add embedded time duration

--- stepper/src/stepper.rs
+++ stepper/src/stepper.rs
@@ -1,34 +1,41 @@
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::timer::CountDown;
-// TODO add embedded_time for duration and time conversions
+use embedded_time::duration::*;
 
 struct Stepper<O, T>{
     step: O,
     dir: O,
     steps_per_revolution: u32,
     timer: T,
-    step_delay: f32
+    step_delay: Microseconds
 }
 
 impl <O, T> Stepper<O, T>
-where O: OutputPin, T: CountDown,
+where O: OutputPin, T: CountDown<Time = Microseconds>,
 {
     pub fn new(step: O, dir: O, steps_per_revolution: u32, timer: T) -> Stepper<O,T>{
         Stepper{
-            step, dir, steps_per_revolution, timer, step_delay: 1000.0
+            step, dir, steps_per_revolution, timer, step_delay: sps_from_rpm(1, steps_per_revolution)
         }
     }
 
     pub fn set_speed(&mut self, rpm: u32) -> (){
-        let rps = (rpm / 60) as f32;
-        let spr = 1.0 / rps;
-        self.step_delay = spr / (self.steps_per_revolution as f32);
+        self.step_delay = sps_from_rpm(rpm, self.steps_per_revolution);
     }
 
     pub fn step(&mut self) -> (){
-        self.timer.start();
-        self.step.set_high();
+        let _ = self.step.set_high();
+        self.timer.start(self.step_delay);
         self.timer.wait().unwrap();
-        self.step.set_low();
+        let _ = self.step.set_low();
     }
+}
+
+// get second per step from round per minute
+fn sps_from_rpm(rpm: u32, steps_per_revolution: u32) -> Microseconds<u32> {
+    let rps = (rpm / 60) as f32;
+    let spr = 1.0 / rps;
+    let sps = spr/(steps_per_revolution as f32);
+    let microsps = (sps * 1_000_000.0) as u32;
+    return Microseconds(microsps);
 }
