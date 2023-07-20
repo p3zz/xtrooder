9ee5ce2b74ee93141116316b7791350d10d3a769
p3zz <federico.pezzato.work@gmail.com>
2023-07-20 20:50:44 +0200

    add task for pwm

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,17 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::peripherals::TIM12;
 use embassy_stm32::pwm::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::pwm::Channel;
 use embassy_stm32::time::{khz, hz};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::task]
+async fn pwm(mut pwm: SimplePwm<'static, TIM12>){
+    pwm.set_freq(hz(10));
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -16,19 +22,19 @@ async fn main(_spawner: Spawner) {
     info!("Hello World!");
 
     let red_led = PwmPin::new_ch1(p.PB14);
-    let mut pwm = SimplePwm::new(p.TIM12, Some(red_led), None, None, None, hz(1));
-    let max = pwm.get_max_duty();
-    pwm.enable(Channel::Ch1);
-    pwm.set_duty(Channel::Ch1, max/2);
+
+    let mut simplepwm = SimplePwm::new(p.TIM12, Some(red_led), None, None, None, hz(1));
+
+    let max = simplepwm.get_max_duty();
+    simplepwm.enable(Channel::Ch1);
+    simplepwm.set_duty(Channel::Ch1, max/2);
+
+    let _ = _spawner.spawn(pwm(simplepwm));
 
     info!("PWM initialized");
 
     loop {
-        info!("Slow for 1 second");
-        pwm.set_freq(hz(10));
+        info!("Main loop");
         Timer::after(Duration::from_millis(1000)).await;
-        info!("Fast for 2 seconds");
-        pwm.set_freq(hz(20));
-        Timer::after(Duration::from_millis(2000)).await;
     }
 }
