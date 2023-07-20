571a3e07b852cf7507d8b02e9df55810f9dbdc0d
p3zz <federico.pezzato.work@gmail.com>
2023-07-20 19:56:40 +0200

    add working pwm exaple

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,31 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed, AnyPin};
-use embassy_time::{Duration, Timer};
-use {defmt_rtt as _, panic_probe as _};
-
 use embassy_stm32::pwm::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::pwm::Channel;
-
-#[embassy_executor::task(pool_size=2)]
-async fn blink(mut pin: Output<'static, AnyPin>, duration: Duration){
-    loop {
-        info!("high");
-        pin.set_high();
-        Timer::after(duration).await;
-
-        info!("low");
-        pin.set_low();
-        Timer::after(duration).await;
-    }
-}
-
+use embassy_stm32::time::{khz, hz};
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
 
-    let red = Output::new(p.PB14, Level::High, Speed::Low);
+    let red_led = PwmPin::new_ch1(p.PB14);
+    let mut pwm = SimplePwm::new(p.TIM12, Some(red_led), None, None, None, hz(1));
+    let max = pwm.get_max_duty();
+    pwm.enable(Channel::Ch1);
+    pwm.set_duty(Channel::Ch1, max/2);
 
-    let green = Output::new(p.PB0, Level::High, Speed::Low);
+    info!("PWM initialized");
 
-    let _ =_spawner.spawn(blink(red.degrade(), Duration::from_millis(200)));
-    let _ =_spawner.spawn(blink(green.degrade(), Duration::from_millis(500)));
-
-    loop{
+    loop {
+        info!("Slow for 1 second");
+        pwm.set_freq(hz(10));
+        Timer::after(Duration::from_millis(1000)).await;
+        info!("Fast for 2 seconds");
+        pwm.set_freq(hz(20));
         Timer::after(Duration::from_millis(2000)).await;
-        info!("Hello World!");
     }
-    
 }
