669612648db00867172efa37544fb99ecf4c4551
p3zz <federico.pezzato.work@gmail.com>
2023-07-25 15:58:57 +0200

    use stepper driver inside main

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,14 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::Peripheral;
-use embassy_stm32::gpio::{Output, Level, Speed, AnyPin};
-use embassy_stm32::peripherals::{TIM3, TIM5, PA6};
 use embassy_stm32::pwm::simple_pwm::{PwmPin, SimplePwm};
-use embassy_stm32::pwm::{Channel, CaptureCompare16bitInstance};
-use embassy_stm32::time::{khz, hz, Hertz};
-use embassy_stm32::timer::GeneralPurpose16bitInstance;
+use embassy_stm32::pwm::Channel;
+use embassy_stm32::time::hz;
 use embassy_time::{Duration, Timer};
 use futures::join;
 use {defmt_rtt as _, panic_probe as _};
 
-// mod stepper;
-
-async fn move_for<T: CaptureCompare16bitInstance>(pwm: &mut SimplePwm<'_, T>, duration: Duration, f: Hertz) {
-    pwm.enable(Channel::Ch1);
-    pwm.set_freq(f);
-    Timer::after(duration).await;
-    pwm.disable(Channel::Ch1)
-}
+mod stepper;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -35,21 +24,19 @@ async fn main(_spawner: Spawner) {
     let mut red_pwm = SimplePwm::new(p.TIM3, Some(PwmPin::new_ch1(p.PA6)),
         None, None, None, hz(1));
     let red_max = red_pwm.get_max_duty();
-    red_pwm.enable(Channel::Ch1);
     red_pwm.set_duty(Channel::Ch1, red_max/2);
+
+    let mut red_stepper = stepper::stepper::Stepper::new(red_pwm);
 
     let mut green_pwm = SimplePwm::new(p.TIM5, Some(PwmPin::new_ch1(p.PA0)),
         None, None, None, hz(1));
     let green_max = green_pwm.get_max_duty();
-    green_pwm.enable(Channel::Ch1);
     green_pwm.set_duty(Channel::Ch1, green_max/2);
 
-    info!("PWM initialized");
+    let mut green_stepper = stepper::stepper::Stepper::new(green_pwm);
 
     loop {
-        info!("Main loop");
-        join!(move_for(&mut red_pwm, Duration::from_millis(500), hz(20)),
-            move_for(&mut green_pwm, Duration::from_millis(500), hz(10)));
+        join!(red_stepper.step(), green_stepper.step());
         Timer::after(Duration::from_millis(500)).await;
     }
 }
--- src/stepper/stepper.rs
+++ src/stepper/stepper.rs
@@ -1,7 +1,9 @@
 #![no_std]
 
-use embedded_hal::digital::v2::OutputPin;
-use embedded_hal::timer::CountDown;
+use embassy_stm32::pwm::{CaptureCompare16bitInstance, Channel};
+use embassy_stm32::pwm::simple_pwm::SimplePwm;
+use embassy_stm32::time::hz;
+use embassy_time::{Timer, Duration};
 
 pub struct Length{
     // mm
@@ -25,75 +27,79 @@ pub enum StepperDirection{
     CounterClockwise
 }
 
-pub struct Stepper<S, D, T>{
-    step: S,
-    dir: D,
-    steps_per_revolution: u32,
-    timer: T,
-    step_delay: MicroSeconds,
+pub struct Stepper<'d, T>{
+    step: SimplePwm<'d, T>,
+    // dir: D,
+    // steps_per_revolution: u32,
+    // timer: T,
+    // step_delay: MicroSeconds,
     // mm
-    distance_per_step: Length,
+    // distance_per_step: Length,
     // mm
-    position: Length,
-    direction: StepperDirection
+    // position: Length,
+    // direction: StepperDirection
 }
 
-impl <S, D, T> Stepper<S, D, T>
-where S: OutputPin, D: OutputPin, T: CountDown<Time = Hertz>,
+impl <'d, T> Stepper<'d, T>
+where T: CaptureCompare16bitInstance,
 {
-    pub fn new(step: S, dir: D, steps_per_revolution: u32, timer: T, distance_per_step: Length) -> Stepper<S, D, T>{
+    pub fn new(step: SimplePwm<'d, T>) -> Stepper<T>{
         Stepper{
             step,
-            dir,
-            steps_per_revolution,
-            timer,
-            step_delay: sps_from_rpm(1, steps_per_revolution),
-            distance_per_step,
-            position: Length::from_mm(0.0),
-            direction: StepperDirection::Clockwise
+            // dir,
+            // steps_per_revolution,
+            // timer,
+            // step_delay: sps_from_rpm(1, steps_per_revolution),
+            // distance_per_step,
+            // position: Length::from_mm(0.0),
+            // direction: StepperDirection::Clockwise
         }
     }
 
-    pub fn set_speed(&mut self, speed: u32) -> (){
-        self.step_delay = sps_from_rpm(speed, self.steps_per_revolution);
+    // pub fn set_speed(&mut self, speed: u32) -> (){
+    //     self.step_delay = sps_from_rpm(speed, self.steps_per_revolution);
+    // }
+
+    // pub fn set_direction(&mut self, direction: StepperDirection) -> (){
+    //     self.direction = direction;
+    //     let _  = match self.direction {
+    //         StepperDirection::Clockwise => self.dir.set_high(),
+    //         StepperDirection::CounterClockwise => self.dir.set_low()
+    //     };
+    // }
+
+    pub async fn step(&mut self) -> (){
+        self.step.enable(Channel::Ch1);
+        self.step.set_freq(hz(10));
+        Timer::after(Duration::from_millis(500)).await;
+        self.step.disable(Channel::Ch1);
+        // let _ = self.step.set_high();
+        // self.timer.start(self.step_delay);
+        // block!(self.timer.wait()).unwrap();
+        // let _ = self.step.set_low();
+        // let distance = match self.direction{
+            // StepperDirection::Clockwise => self.distance_per_step.to_mm(),
+            // StepperDirection::CounterClockwise => -self.distance_per_step.to_mm()
+        // };
+        // self.position = Length::from_mm(self.position.to_mm() + distance);
     }
 
-    pub fn set_direction(&mut self, direction: StepperDirection) -> (){
-        self.direction = direction;
-        let _  = match self.direction {
-            StepperDirection::Clockwise => self.dir.set_high(),
-            StepperDirection::CounterClockwise => self.dir.set_low()
-        };
-    }
-
-    pub fn step(&mut self) -> (){
-        let _ = self.step.set_high();
-        self.timer.start(self.step_delay);
-        block!(self.timer.wait()).unwrap();
-        let _ = self.step.set_low();
-        let distance = match self.direction{
-            StepperDirection::Clockwise => self.distance_per_step.to_mm(),
-            StepperDirection::CounterClockwise => -self.distance_per_step.to_mm()
-        };
-        self.position = Length::from_mm(self.position.to_mm() + distance);
-    }
-
-    pub fn move_for(&mut self, distance: Length) -> (){
-        let steps = (distance.to_mm() / self.distance_per_step.to_mm()) as u32;
-        for _ in 0..steps{
-            self.step();
-        }
-    }
+    // pub fn move_for(&mut self, distance: Length) -> (){
+    //     let steps = (distance.to_mm() / self.distance_per_step.to_mm()) as u32;
+    //     for _ in 0..steps{
+    //         self.step();
+    //     }
+    // }
 }
 
 // get second per step from round per minute
-fn sps_from_rpm(rpm: u32, steps_per_revolution: u32) -> MicroSeconds {
-    let rps = (rpm / 60) as f32;
-    let spr = 1.0 / rps;
-    let sps = spr/(steps_per_revolution as f32);
-    let microsps = (sps * 1_000_000.0) as u32;
-    return MicroSeconds(microsps);
-}
+// fn sps_from_rpm(rpm: u32, steps_per_revolution: u32) -> MicroSeconds {
+//     let rps = (rpm / 60) as f32;
+//     let spr = 1.0 / rps;
+//     let sps = spr/(steps_per_revolution as f32);
+//     let microsps = (sps * 1_000_000.0) as u32;
+//     return MicroSeconds(microsps);
+// }
 
 // get distance per step from pulley's radius
 // used for X/Y axis
