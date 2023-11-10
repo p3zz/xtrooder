eb39eb06dae90700385da18172e549912eea9fd0
p3zz <federico.pezzato.work@gmail.com>
2023-11-10 16:50:02 +0100

    improve a4988 driver

--- src/stepper/a4988.rs
+++ src/stepper/a4988.rs
@@ -24,10 +24,9 @@ pub struct Stepper<'s, 'd, S>{
     dir: Output<'d, AnyPin>,
     steps_per_revolution: u64,
     distance_per_step: Length,
-    speed: Speed,
     position: Position,
     direction: StepperDirection,
-    step_delay: Duration
+    step_duration: Duration,
 }
 
 impl <'s, 'd, S> Stepper <'s, 'd, S>
@@ -35,22 +34,26 @@ where S: CaptureCompare16bitInstance,
 {
     pub fn new(step: SimplePwm<'s, S>, dir: Output<'d, AnyPin>, steps_per_revolution: u64, radius: Length) -> Stepper<'s, 'd, S>{
         let distance_per_step = dps_from_radius(radius, steps_per_revolution);
-        let speed = Speed::from_mmps(0.0).unwrap();
         Stepper{
             step,
             dir,
             steps_per_revolution,
-            speed,
             distance_per_step,
             position: Position::from_mm(0.0),
             direction: StepperDirection::Clockwise,
-            step_delay: compute_step_delay(steps_per_revolution, distance_per_step, speed)
+            step_duration: compute_step_duration(steps_per_revolution, distance_per_step, Speed::from_mmps(0.0).unwrap())
         }
     }
 
+    /*
+    update the speed an dcompute the frequency in which the pwm must run.
+    pwm frequency: count of PWM interval periods per second
+    PWM period: duration of one complete cycle or the total amount of active and inactive time combined
+    */ 
     pub fn set_speed(&mut self, speed: Speed) -> (){
-        self.speed = speed;
-        self.step_delay = compute_step_delay(self.steps_per_revolution, self.distance_per_step, self.speed);
+        self.step_duration = compute_step_duration(self.steps_per_revolution, self.distance_per_step, speed);
+        let freq = hz(((1.0 / self.step_duration.as_micros() as f64) * 1_000_000.0) as u32);
+        self.step.set_freq(freq);
     }
 
     pub fn set_direction(&mut self, direction: StepperDirection) -> (){
@@ -62,7 +65,7 @@ where S: CaptureCompare16bitInstance,
     }
 
     // the stepping is implemented through a pwm, where the duty is 50% (in order to have high/low pin for the same amount of time),
-    // and the frequency is computed using the time for a step to be executed (step delay)
+    // and the frequency is computed using the time for a step to be executed (step duration)
     pub async fn move_for(&mut self, distance: Length){
         if distance.to_mm() < self.distance_per_step.to_mm() {
             return;
@@ -70,26 +73,15 @@ where S: CaptureCompare16bitInstance,
         // compute the number of steps we need to perform
         let steps_n = (distance.to_mm() / self.distance_per_step.to_mm()) as u64;
 
-        /*
-        compute the duration of the move.
-        In order to perform a single step, we need to set the step pin to HIGH
-        for step_delay, then reset it to LOW for step_delay
-         */ 
-        let duration = Duration::from_micros(2 * steps_n * self.step_delay.as_micros());
-
-        /*
-        compute the frequency in which the pwm must run.
-        pwm frequency: count of PWM interval periods per second
-        PWM period: duration of one complete cycle or the total amount of active and inactive time combined
-         */ 
-        let freq = hz(((1.0 / self.step_delay.as_micros() as f64) * 1_000_000.0) as u32);
+        // compute the duration of the move.
+        let duration = Duration::from_micros(steps_n * self.step_duration.as_micros());
         
-        // execute the pwm
+        // move
         self.step.enable(Channel::Ch1);
-        self.step.set_freq(freq);
         Timer::after(duration).await;
         self.step.disable(Channel::Ch1);
         
+        // update the position of the stepper
         let distance = match self.direction{
             StepperDirection::Clockwise => distance.to_mm(),
             StepperDirection::CounterClockwise => -distance.to_mm()
@@ -126,15 +118,16 @@ pub fn dps_from_pitch(pitch: Length, steps_per_revolution: u64) -> Length {
     Length::from_mm(pitch.to_mm() / (steps_per_revolution as f64)).unwrap()
 }
 
-// compute the step delay, known as the time taken to perform a single step (active + inactive time)
+// compute the step duration, known as the time taken to perform a single step (active + inactive time)
 // spr -> step per revolution
 // dps -> distance per step
-fn compute_step_delay(spr: u64, dps: Length, speed: Speed) -> Duration {
+fn compute_step_duration(spr: u64, dps: Length, speed: Speed) -> Duration {
     // distance per revolution
     let distance_per_revolution = Length::from_mm(spr as f64 * dps.to_mm()).unwrap();
     let revolution_per_second = speed.to_mmps() / distance_per_revolution.to_mm();
     let second_per_revolution = 1.0 / revolution_per_second;
     let second_per_step = second_per_revolution / (spr as f64);
     let usecond_per_step = (second_per_step * 1_000_000.0) as u64;
-    Duration::from_micros(usecond_per_step)
+    // we have to take into account also the time the stepper in inactive, so multiply the us per 2
+    Duration::from_micros(usecond_per_step * 2)
 }
