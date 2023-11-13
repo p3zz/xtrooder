97b2cdec42d2f67a6b603f1432eac712d6ea598e
p3zz <federico.pezzato.work@gmail.com>
2023-11-13 21:26:44 +0100

    [WIP] add relative motion

--- src/planner/motion.rs
+++ src/planner/motion.rs
@@ -60,6 +60,26 @@ pub async fn linear_move<'s, S: CaptureCompare16bitInstance>(
     stepper.move_to(dest).await;
 }
 
+pub async fn linear_move_for<'s, S: CaptureCompare16bitInstance>(
+    stepper: &mut Stepper<'s, S>,
+    distance: Position,
+    feedrate: Speed,
+) {
+    let dest = stepper.get_position().add(distance);
+    linear_move(stepper, dest, feedrate).await;
+}
+
+pub async fn linear_move_for_2d<'s, A: CaptureCompare16bitInstance, B: CaptureCompare16bitInstance>(
+    stepper_a: &mut Stepper<'s, A>,
+    stepper_b: &mut Stepper<'s, B>,
+    distance: Position2D,
+    feedrate: Speed,
+) {
+    let dest_a = stepper_a.get_position().add(distance.get_x());
+    let dest_b = stepper_b.get_position().add(distance.get_y());
+    linear_move_2d(stepper_a, stepper_b, Position2D::new(dest_a, dest_b), feedrate).await;
+}
+
 pub async fn linear_move_e<'s, A: CaptureCompare16bitInstance, E: CaptureCompare16bitInstance>(
     stepper_a: &mut Stepper<'s, A>,
     stepper_e: &mut Stepper<'s, E>,
@@ -79,4 +99,35 @@ pub async fn linear_move_e<'s, A: CaptureCompare16bitInstance, E: CaptureCompare
         linear_move(stepper_a, dest, feedrate),
         linear_move(stepper_e, e_dest, e_speed)
     );
+}
+
+pub async fn linear_move_for_e
+<'s, A: CaptureCompare16bitInstance, E: CaptureCompare16bitInstance>(
+    stepper_a: &mut Stepper<'s, A>,
+    stepper_e: &mut Stepper<'s, E>,
+    distance: Position,
+    e_dest: Position,
+    feedrate: Speed,
+) {
+    let dest = stepper_a.get_position().add(distance);
+    linear_move_e(stepper_a, stepper_e, dest, e_dest, feedrate).await;
+}
+
+pub async fn linear_move_for_2d_e<
+    's,
+    A: CaptureCompare16bitInstance,
+    B: CaptureCompare16bitInstance,
+    E: CaptureCompare16bitInstance,
+>(
+    stepper_a: &mut Stepper<'s, A>,
+    stepper_b: &mut Stepper<'s, B>,
+    stepper_e: &mut Stepper<'s, E>,
+    ab_dest: Position2D,
+    e_dest: Position,
+    feedrate: Speed,
+) {
+    let dest_a = stepper_a.get_position().add(ab_dest.get_x());
+    let dest_b = stepper_b.get_position().add(ab_dest.get_y());
+    let dest = Position2D::new(dest_a, dest_b);
+    linear_move_2d_e(stepper_a, stepper_b, stepper_e, dest, e_dest, feedrate).await;
 }
