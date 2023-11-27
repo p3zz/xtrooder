cafc667d2655f131476bc27b159922a8007d799c
p3zz <federico.pezzato.work@gmail.com>
2023-11-27 22:29:47 +0100

    refactor thermistor computation

--- src/hotend/thermistor.rs
+++ src/hotend/thermistor.rs
@@ -4,6 +4,7 @@ use micromath::F32Ext;
 
 use {defmt_rtt as _, panic_probe as _};
 
+#[derive(Clone, Copy)]
 struct Temperature{
     // unit: C (celsius)
     value: f64
@@ -37,38 +38,32 @@ struct Thermistor<'a, T, P>
 where T: Instance, P: AdcPin<T> + Pin{
     adc: Adc<'a, T>,
     read_pin: P,
-    resolution: Resolution
+    resolution: Resolution,
+    r0: f64,
+    b: Temperature
 }
 
 impl <'a, T, P> Thermistor<'a, T, P>
 where T: Instance, P: AdcPin<T> + Pin {
-    pub fn new(mut adc: Adc<'a, T>, read_pin: P, resolution: Resolution) -> Thermistor<'a, T, P>{
+    pub fn new(mut adc: Adc<'a, T>, read_pin: P, resolution: Resolution, r0: f64, b: Temperature) -> Thermistor<'a, T, P>{
         adc.set_sample_time(SampleTime::Cycles32_5);
         adc.set_resolution(resolution);
         Thermistor{
             adc,
             read_pin,
-            resolution
+            resolution,
+            r0,
+            b
         }
     }
 
     pub fn read_temperature(&mut self) -> Temperature{
-        let adc_res_n = get_resolution(self.resolution);
-        let r0 = 10_000.0;
-        let b = 3950.0;
-        let room_temperature = Temperature::from_kelvin(298.15);
-
-        let sample = self.adc.read(&mut self.read_pin) as f64;
-        
-        let r = r0 * ((adc_res_n - 1.0) / sample - 1.0);
-
-        let temperature_val = 1.0 / ((1.0 / room_temperature.to_kelvin() as f32) + (1.0 / b) * ((r as f32 / r0 as f32).ln())) as f64;
-
-        Temperature::from_kelvin(temperature_val)
+        let sample = self.adc.read(&mut self.read_pin) as f64;        
+        compute_temperature(sample, self.resolution, Temperature::from_kelvin(298.15), self.b, self.r0)
     }
 }
 
-fn get_resolution(resolution: Resolution) -> f64{
+fn get_steps(resolution: Resolution) -> f64{
     match resolution{
         Resolution::SixteenBit => 65536.0,
         Resolution::FourteenBit => 16384.0,
@@ -76,4 +71,12 @@ fn get_resolution(resolution: Resolution) -> f64{
         Resolution::TenBit => 1024.0,
         Resolution::EightBit => 256.0,
     }
+}
+
+// Steinhart–Hart equation simplified for ntc thermistors
+fn compute_temperature(sample: f64, resolution: Resolution, t0: Temperature, b: Temperature, r0: f64) -> Temperature {
+    let resolution_steps = get_steps(resolution);
+    let r = r0 * ((resolution_steps - 1.0) / sample - 1.0);
+    let val_inv = (1.0 / t0.to_kelvin()) + (1.0 / b.to_kelvin()) * (((r / r0) as f32).ln() as f64);
+    Temperature::from_kelvin(1.0 / val_inv)
 }
