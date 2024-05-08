f9ada5f68cdc4c84f0c3af17094c6d57cba99a6f
p3zz <federico.pezzato.work@gmail.com>
2024-05-08 17:14:26 +0200

    add linear3d motion tests

--- cross/app/src/lib.rs
+++ cross/app/src/lib.rs
@@ -11,14 +11,14 @@ pub mod utils;
 mod tests {
     use defmt_rtt as _;
     use embassy_stm32::gpio::{Level, Output, Speed as PinSpeed};
-    use math::{common::RotationDirection, distance::Distance, speed::Speed, vector::Vector2D};
+    use math::{common::RotationDirection, distance::Distance, speed::Speed, vector::{Vector2D, Vector3D}};
     use panic_probe as _;
     use defmt::assert;
 
     use crate::{planner::motion, stepper::a4988::{Stepper, StepperAttachment, StepperOptions, SteppingMode}};
 
     #[init]
-    fn init() -> (Stepper<'static>, Stepper<'static>) {
+    fn init() -> (Stepper<'static>, Stepper<'static>, Stepper<'static>) {
         let p = embassy_stm32::init(embassy_stm32::Config::default());
 
         let step = Output::new(p.PA0, Level::Low, PinSpeed::Low);
@@ -43,12 +43,23 @@ mod tests {
             None
         );
 
-        (a_stepper, b_stepper)
+        let step = Output::new(p.PA2, Level::Low, PinSpeed::Low);
+
+        let dir = Output::new(p.PB2, Level::Low, PinSpeed::Low);
+
+        let c_stepper = Stepper::new(
+            step,
+            dir,
+            StepperOptions::default(),
+            None
+        );
+
+        (a_stepper, b_stepper, c_stepper)
 
     }
 
     #[test]
-    fn test_stepper_move_clockwise(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_clockwise(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 20;
         s.0.reset();
         s.0.set_direction(RotationDirection::Clockwise);
@@ -59,7 +70,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_counterclockwise(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_counterclockwise(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 20;
         s.0.reset();
         s.0.set_direction(RotationDirection::CounterClockwise);
@@ -69,7 +80,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_microstepping_clockwise(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_microstepping_clockwise(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 20;
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::HalfStep);
@@ -80,7 +91,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_microstepping_counterclockwise(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_microstepping_counterclockwise(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 20;
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::HalfStep);
@@ -91,7 +102,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_clockwise_positive_direction_clockwise(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_clockwise_positive_direction_clockwise(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 20;
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
@@ -105,7 +116,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_clockwise_positive_direction_counterclockwise(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_clockwise_positive_direction_counterclockwise(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 20;
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
@@ -119,7 +130,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_counterclockwise_positive_direction_clockwise(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_counterclockwise_positive_direction_clockwise(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 20;
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
@@ -133,7 +144,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_counterclockwise_positive_direction_counterclockwise(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_counterclockwise_positive_direction_counterclockwise(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 20;
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
@@ -147,7 +158,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_for_distance_no_attachment(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_for_distance_no_attachment(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let distance = Distance::from_mm(20.0);
         s.0.reset();
         let res = s.0.move_for_distance(distance);
@@ -155,7 +166,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_for_distance(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_for_distance(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let distance = Distance::from_mm(10.0);
         s.0.reset();
         s.0.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(1.0) });
@@ -167,7 +178,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_for_distance_space_wasted(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_for_distance_space_wasted(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let distance = Distance::from_mm(10.5);
         s.0.reset();
         s.0.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(1.0) });
@@ -179,7 +190,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_for_distance_lower_distance_per_step(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_for_distance_lower_distance_per_step(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let distance = Distance::from_mm(10.5);
         s.0.reset();
         s.0.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(0.5) });
@@ -191,7 +202,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_move_for_steps_outofbounds(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_move_for_steps_outofbounds(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 10;
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
@@ -210,7 +221,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_home(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_home(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let steps = 10;
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
@@ -227,7 +238,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_home_no_attachment(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_home_no_attachment(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
 
@@ -237,7 +248,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_set_speed_positive(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_set_speed_positive(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
         let res = s.0.set_speed(1.0);
@@ -246,7 +257,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_set_speed_zero(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_set_speed_zero(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
         let res = s.0.set_speed(0.0);
@@ -254,7 +265,7 @@ mod tests {
     }
 
     #[test]
-    fn test_stepper_set_speed_negative(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_stepper_set_speed_negative(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         s.0.reset();
         s.0.set_stepping_mode(SteppingMode::FullStep);
         let res = s.0.set_speed(-10.0);
@@ -267,7 +278,7 @@ mod tests {
     }
 
     #[test]
-    fn test_linear_move_to(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_linear_move_to(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let destination = Distance::from_mm(10.0);
         let speed = Speed::from_mm_per_second(10.0);
         s.0.reset();
@@ -282,7 +293,7 @@ mod tests {
     }
 
     #[test]
-    fn test_linear_move_to_negative_speed(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_linear_move_to_negative_speed(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let destination = Distance::from_mm(-10.0);
         let speed = Speed::from_mm_per_second(-10.0);
         s.0.reset();
@@ -295,7 +306,7 @@ mod tests {
     }
 
     #[test]
-    fn test_linear_move_to_2d(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_linear_move_to_2d(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let destination = Vector2D::new(Distance::from_mm(-10.0), Distance::from_mm(-10.0));
         let speed = Speed::from_mm_per_second(-10.0);
         s.0.reset();
@@ -316,7 +327,7 @@ mod tests {
     }
 
     #[test]
-    fn test_linear_move_to_2d_2(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_linear_move_to_2d_2(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let destination = Vector2D::new(Distance::from_mm(-5.0), Distance::from_mm(5.0));
         let speed = Speed::from_mm_per_second(10.0);
         s.0.reset();
@@ -336,7 +347,7 @@ mod tests {
     }
 
     #[test]
-    fn test_linear_move_to_2d_different_stepping_mode(s: &mut (Stepper<'static>, Stepper<'static>)) {
+    fn test_linear_move_to_2d_different_stepping_mode(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
         let destination = Vector2D::new(Distance::from_mm(-5.0), Distance::from_mm(5.0));
         let speed = Speed::from_mm_per_second(10.0);
         s.0.reset();
@@ -356,5 +367,62 @@ mod tests {
         assert_eq!(s.0.get_speed_from_attachment().unwrap().to_mm_per_second(), 7.07734118446382);
         assert_eq!(s.1.get_speed_from_attachment().unwrap().to_mm_per_second(), 7.074337134610486);
     }
+
+    #[test]
+    fn test_linear_move_to_3d(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
+        let destination = Vector3D::new(Distance::from_mm(-5.0), Distance::from_mm(5.0), Distance::from_mm(5.0));
+        let speed = Speed::from_mm_per_second(10.0);
+        s.0.reset();
+        s.1.reset();
+        s.2.reset();
+        s.0.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(1.0) });
+        s.1.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(1.0) });
+        s.2.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(1.0) });
+        s.0.set_stepping_mode(SteppingMode::FullStep);
+        s.1.set_stepping_mode(SteppingMode::FullStep);
+        s.2.set_stepping_mode(SteppingMode::FullStep);
+        let res = motion::linear_move_to_3d(&mut s.0, &mut s.1, &mut s.2, destination, speed);
+        assert!(res.is_ok());
+        assert_eq!(s.0.get_steps(), -5.0);
+        assert_eq!(s.1.get_steps(), 5.0);
+        assert_eq!(s.2.get_steps(), 5.0);
+        assert_eq!(s.0.get_position().unwrap().to_mm(), -5.0);
+        assert_eq!(s.1.get_position().unwrap().to_mm(), 5.0);
+        assert_eq!(s.2.get_position().unwrap().to_mm(), 5.0);
+        assert_eq!(s.0.get_direction(), RotationDirection::CounterClockwise);
+        assert_eq!(s.1.get_direction(), RotationDirection::Clockwise);
+        assert_eq!(s.2.get_direction(), RotationDirection::Clockwise);
+        assert_eq!(s.0.get_speed_from_attachment().unwrap().to_mm_per_second(), 7.07734118446382);
+        assert_eq!(s.1.get_speed_from_attachment().unwrap().to_mm_per_second(), 7.07734118446382);
+        assert_eq!(s.2.get_speed_from_attachment().unwrap().to_mm_per_second(), 7.07734118446382);
+    }
     
+    #[test]
+    fn test_linear_move_to_3d_lower_distance_per_step(s: &mut (Stepper<'static>, Stepper<'static>, Stepper<'static>)) {
+        let destination = Vector3D::new(Distance::from_mm(-5.0), Distance::from_mm(-2.0), Distance::from_mm(5.0));
+        let speed = Speed::from_mm_per_second(10.0);
+        s.0.reset();
+        s.1.reset();
+        s.2.reset();
+        s.0.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(0.5) });
+        s.1.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(0.5) });
+        s.2.set_attachment(StepperAttachment { distance_per_step: Distance::from_mm(0.5) });
+        s.0.set_stepping_mode(SteppingMode::FullStep);
+        s.1.set_stepping_mode(SteppingMode::FullStep);
+        s.2.set_stepping_mode(SteppingMode::FullStep);
+        let res = motion::linear_move_to_3d(&mut s.0, &mut s.1, &mut s.2, destination, speed);
+        assert!(res.is_ok());
+        assert_eq!(s.0.get_steps(), -10.0);
+        assert_eq!(s.1.get_steps(), -4.0);
+        assert_eq!(s.2.get_steps(), 10.0);
+        assert_eq!(s.0.get_position().unwrap().to_mm(), -5.0);
+        assert_eq!(s.1.get_position().unwrap().to_mm(), -2.0);
+        assert_eq!(s.2.get_position().unwrap().to_mm(), 5.0);
+        assert_eq!(s.0.get_direction(), RotationDirection::CounterClockwise);
+        assert_eq!(s.1.get_direction(), RotationDirection::CounterClockwise);
+        assert_eq!(s.2.get_direction(), RotationDirection::Clockwise);
+        assert_eq!(s.0.get_speed_from_attachment().unwrap().to_mm_per_second(), 9.277470590418229);
+        assert_eq!(s.1.get_speed_from_attachment().unwrap().to_mm_per_second(), 3.725338260714073);
+        assert_eq!(s.2.get_speed_from_attachment().unwrap().to_mm_per_second(), 7.07734118446382);
+    }
 }
