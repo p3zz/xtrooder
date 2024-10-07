e6c2295a5fa0c09f7f5f173c32d5bb8c63023eed
p3zz <federico.pezzato.work@gmail.com>
2024-10-07 23:07:37 +0200

    add at parser tests

--- host/parser/src/at.rs
+++ host/parser/src/at.rs
@@ -2,14 +2,14 @@ use core::str::FromStr;
 
 use heapless::{String, Vec};
 
-pub static AT_TOKEN_PING: &'static str = "AT";
-pub static AT_TOKEN_RESET: &'static str = "RESET";
-pub static AT_TOKEN_VERSION: &'static str = "VERSION";
-pub static AT_TOKEN_ADDRESS: &'static str = "ADDR";
-pub static AT_TOKEN_NAME: &'static str = "NAME";
-pub static AT_TOKEN_ROLE: &'static str = "ROLE";
-pub static AT_TOKEN_UART: &'static str = "UART";
-pub static AT_ENDLINE: &'static str = "\r\n";
+static AT_TOKEN_PING: &'static str = "AT";
+static AT_TOKEN_RESET: &'static str = "RESET";
+static AT_TOKEN_VERSION: &'static str = "VERSION";
+static AT_TOKEN_ADDRESS: &'static str = "ADDR";
+static AT_TOKEN_NAME: &'static str = "NAME";
+static AT_TOKEN_ROLE: &'static str = "ROLE";
+static AT_TOKEN_UART: &'static str = "UART";
+static AT_ENDLINE: &'static str = "\r\n";
 
 pub enum ATToken{
     Ping,
@@ -40,18 +40,16 @@ enum ATCommand{
 
 pub fn serialize_command_read(token: &ATToken) -> Result<String<32>, ()>{
     let t = serialize_token(token)?;
-    match token{
-        ATToken::Ping => Ok(String::from_str(t).unwrap()),
-        _ => {
-            let mut result: String<32> = String::new();
-            result.push_str(AT_TOKEN_PING).unwrap();
-            result.push('+').unwrap();
-            result.push_str(t).unwrap();
+    let mut result: String<32> = String::from_str(AT_TOKEN_PING).unwrap();
+    if !matches!(token, ATToken::Ping){
+        result.push('+').unwrap();
+        result.push_str(t).unwrap();
+        if !matches!(token, ATToken::Reset){
             result.push('?').unwrap();
-            result.push_str(AT_ENDLINE).unwrap();
-            Ok(result)
         }
     }
+    result.push_str(&AT_ENDLINE).unwrap();
+    Ok(result)
 }
 
 // fn serialize_command_set(cmd: ATCommand) -> String<32>{
@@ -77,5 +75,50 @@ fn serialize_token(token: &ATToken) -> Result<&'static str, ()>{
         ATToken::Role => Ok(AT_TOKEN_ROLE),
         ATToken::Uart => Ok(AT_TOKEN_UART),
         _ => Err(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialize_token_ping(){
+        let token = ATToken::Ping;
+        let result = serialize_command_read(&token);
+        assert!(result.is_ok());
+        assert_eq!("AT\r\n", result.unwrap().as_str());
+    }
+
+    #[test]
+    fn test_serialize_token_reset(){
+        let token = ATToken::Reset;
+        let result = serialize_command_read(&token);
+        assert!(result.is_ok());
+        assert_eq!("AT+RESET\r\n", result.unwrap().as_str());
+    }
+
+    #[test]
+    fn test_serialize_token_name(){
+        let token = ATToken::Name;
+        let result = serialize_command_read(&token);
+        assert!(result.is_ok());
+        assert_eq!("AT+NAME?\r\n", result.unwrap().as_str());
+    }
+
+    #[test]
+    fn test_serialize_token_address(){
+        let token = ATToken::Address;
+        let result = serialize_command_read(&token);
+        assert!(result.is_ok());
+        assert_eq!("AT+ADDR?\r\n", result.unwrap().as_str());
+    }
+
+    #[test]
+    fn test_serialize_token_role(){
+        let token = ATToken::Role;
+        let result = serialize_command_read(&token);
+        assert!(result.is_ok());
+        assert_eq!("AT+ROLE?\r\n", result.unwrap().as_str());
     }
 }
