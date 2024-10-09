85c22f38bfaf80258311e76526911200d88c8921
p3zz <federico.pezzato.work@gmail.com>
2024-10-09 14:23:22 +0200

    add docs, update at parser

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 https://www.martyncurrey.com/arduino-with-hc-05-bluetooth-module-at-mode/
 https://www.robotstore.it/Modulo-convertitore-Seriale-Bluetooth-HC-05
+https://components101.com/sites/default/files/component_datasheet/HC-05%20Datasheet.pdf
--- host/parser/src/at.rs
+++ host/parser/src/at.rs
@@ -1,5 +1,5 @@
 use core::str::FromStr;
-
+use core::fmt::Write;
 use heapless::{String, Vec};
 
 static AT_TOKEN_PING: &'static str = "AT";
@@ -11,40 +11,68 @@ static AT_TOKEN_ROLE: &'static str = "ROLE";
 static AT_TOKEN_UART: &'static str = "UART";
 static AT_ENDLINE: &'static str = "\r\n";
 
-pub enum ATToken{
-    Ping,
-    Reset,
-    Version,
-    Address,
-    Name,
-    Role,
-    Uart,
-}
-
-
 enum ATRole{
     Slave = 0,
     Master = 1,
     SlaveLoop = 2
 }
 
-enum ATCommand{
+impl Into<char> for ATRole{
+    fn into(self) -> char {
+        char::from(self as u8 + b'0')
+    }
+}
+
+struct ATVersionParams{
+    value: String<16>
+}
+
+struct ATAddressParams{
+    nap: Vec<u8, 2>,
+    uap: u8,
+    lap: Vec<u8, 3>
+}
+
+struct ATNameParams{
+    value: String<16>
+}
+
+struct ATRoleParams{
+    value: ATRole
+}
+
+struct ATUartParams{
+    baudrate: u64,
+    stop_bit: u64,
+    parity_bit: u64
+}
+
+pub enum ATCommand{
     Ping,
     Reset,
-    Version{value: String<16>},
+    Version(Option<ATVersionParams>),
+    Address(Option<ATAddressParams>),
+    Name(Option<ATNameParams>),
+    Role(Option<ATRoleParams>),
+    Uart(Option<ATUartParams>),
+}
+
+
+enum ATParams{
+    Version,
     Address{nap: Vec<u8, 2>, uap: u8, lap: Vec<u8, 3>},
     Name{value: String<16>},
     Role{value: ATRole},
-    Uart{baudrate: u64, stop_bit: u64, parity_bit: u64}
+    Uart{}
 }
 
-pub fn serialize_command_read(token: &ATToken) -> Result<String<32>, ()>{
+pub fn serialize_command_read(token: &ATCommand) -> Result<String<32>, ()>{
     let t = serialize_token(token)?;
     let mut result: String<32> = String::from_str(AT_TOKEN_PING).unwrap();
-    if !matches!(token, ATToken::Ping){
+    if !matches!(token, ATCommand::Ping){
         result.push('+').unwrap();
         result.push_str(t).unwrap();
-        if !matches!(token, ATToken::Reset){
+        if !matches!(token, ATCommand::Reset){
             result.push('?').unwrap();
         }
     }
@@ -52,28 +80,37 @@ pub fn serialize_command_read(token: &ATToken) -> Result<String<32>, ()>{
     Ok(result)
 }
 
-// fn serialize_command_set(cmd: ATCommand) -> String<32>{
-//     match cmd{
-//         ATCommand::Ping => ,
-//         ATCommand::Reset => todo!(),
-//         ATCommand::Version { value } => todo!(),
-//         ATCommand::Address { nap, uap, lap } => todo!(),
-//         ATCommand::Name { value } => todo!(),
-//         ATCommand::Role { value } => todo!(),
-//         ATCommand::Uart { baudrate, stop_bit, parity_bit } => todo!(),
-//     }
-//     todo!()
-// }
-
-fn serialize_token(token: &ATToken) -> Result<&'static str, ()>{
+pub fn serialize_command_write(token: &ATCommand) -> Result<String<32>, ()>{
+    let result: String<32> = String::new();
+    let mut params: String<16> = String::new();
     match token{
-        ATToken::Ping => Ok(AT_TOKEN_PING),
-        ATToken::Reset => Ok(AT_TOKEN_RESET),
-        ATToken::Version => Ok(AT_TOKEN_VERSION),
-        ATToken::Address => Ok(AT_TOKEN_ADDRESS),
-        ATToken::Name => Ok(AT_TOKEN_NAME),
-        ATToken::Role => Ok(AT_TOKEN_ROLE),
-        ATToken::Uart => Ok(AT_TOKEN_UART),
+        ATCommand::Name(p) => {
+            let param = p.ok_or(())?;
+            params.push_str(param.value.as_str());
+        },
+        ATCommand::Role(p) => {
+            let param = p.ok_or(())?;
+            params.push(param.value.into());
+        },
+        ATCommand::Uart(p) => {
+            let param = p.ok_or(())?;
+            write!(params, "{},{},{}", param.baudrate, param.stop_bit, param.parity_bit);
+        },
+        _ => {
+            return Err(())}
+    };
+    Ok(result)
+}
+
+fn serialize_token(token: &ATCommand) -> Result<&'static str, ()>{
+    match token{
+        ATCommand::Ping => Ok(AT_TOKEN_PING),
+        ATCommand::Reset => Ok(AT_TOKEN_RESET),
+        ATCommand::Version(_) => Ok(AT_TOKEN_VERSION),
+        ATCommand::Address(_) => Ok(AT_TOKEN_ADDRESS),
+        ATCommand::Name(_) => Ok(AT_TOKEN_NAME),
+        ATCommand::Role(_) => Ok(AT_TOKEN_ROLE),
+        ATCommand::Uart(_) => Ok(AT_TOKEN_UART),
         _ => Err(())
     }
 }
@@ -84,7 +121,7 @@ mod tests {
 
     #[test]
     fn test_serialize_token_ping(){
-        let token = ATToken::Ping;
+        let token = ATCommand::Ping;
         let result = serialize_command_read(&token);
         assert!(result.is_ok());
         assert_eq!("AT\r\n", result.unwrap().as_str());
@@ -92,7 +129,7 @@ mod tests {
 
     #[test]
     fn test_serialize_token_reset(){
-        let token = ATToken::Reset;
+        let token = ATCommand::Reset;
         let result = serialize_command_read(&token);
         assert!(result.is_ok());
         assert_eq!("AT+RESET\r\n", result.unwrap().as_str());
@@ -100,7 +137,7 @@ mod tests {
 
     #[test]
     fn test_serialize_token_name(){
-        let token = ATToken::Name;
+        let token = ATCommand::Name(None);
         let result = serialize_command_read(&token);
         assert!(result.is_ok());
         assert_eq!("AT+NAME?\r\n", result.unwrap().as_str());
@@ -108,7 +145,7 @@ mod tests {
 
     #[test]
     fn test_serialize_token_address(){
-        let token = ATToken::Address;
+        let token = ATCommand::Address(None);
         let result = serialize_command_read(&token);
         assert!(result.is_ok());
         assert_eq!("AT+ADDR?\r\n", result.unwrap().as_str());
@@ -116,7 +153,7 @@ mod tests {
 
     #[test]
     fn test_serialize_token_role(){
-        let token = ATToken::Role;
+        let token = ATCommand::Role(None);
         let result = serialize_command_read(&token);
         assert!(result.is_ok());
         assert_eq!("AT+ROLE?\r\n", result.unwrap().as_str());
