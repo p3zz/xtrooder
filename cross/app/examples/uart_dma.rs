403cbbfc0697504a62d15e9f0d3bc3a9eaee0b47
p3zz <federico.pezzato.work@gmail.com>
2024-10-15 16:26:58 +0200

    fix uart dma example

--- cross/app/examples/uart_dma.rs
+++ cross/app/examples/uart_dma.rs
@@ -2,11 +2,10 @@
 #![no_main]
 
 use defmt::*;
-use embassy_executor::{Executor, Spawner};
+use embassy_executor::Spawner;
 use embassy_stm32::usart::{Config as UartConfig, Uart};
 use embassy_stm32::{bind_interrupts, peripherals, usart};
 use heapless::{String, Vec};
-use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
 #[link_section = ".ram_d3"]
@@ -23,13 +22,6 @@ async fn main_task() {
     let config = UartConfig::default();
     let mut uart = Uart::new(p.UART4, p.PC11, p.PC10, Irqs, p.DMA1_CH0, p.DMA1_CH1, config).unwrap();
 
-    // unsafe{
-    //     DMA_BUF[1] = b'a';
-    // }
-
-    // let mut v: Vec<u8, 64> = unsafe { Vec::from_slice(&DMA_BUF).unwrap() };
-    // v[0] = b'a';
-    // let s = String::from_utf8(v).unwrap();
     let tmp = unsafe { &mut DMA_BUF[..] };
 
     loop{
@@ -40,19 +32,14 @@ async fn main_task() {
                 info!("{}", str.as_str());
             },
             Err(e) => error!("{}", e),
-        }
+        };
+        tmp.fill(0u8);
     }
 }
 
-static EXECUTOR: StaticCell<Executor> = StaticCell::new();
-
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     info!("Hello World!");
 
-    let executor = EXECUTOR.init(Executor::new());
-
-    executor.run(|spawner| {
-        unwrap!(spawner.spawn(main_task()));
-    })
+    spawner.spawn(main_task()).unwrap();
 }
